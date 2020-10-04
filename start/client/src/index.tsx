2d67de80f1303a6f0afcfa48fae3733c06eee213
Tim <timcpb@hotmail.com>
2020-10-04 22:41:28 +0100

    use cache in cart component

--- start/client/src/index.tsx
+++ start/client/src/index.tsx
@@ -1,39 +1,55 @@
 import {
+    gql,
     ApolloClient,
     InMemoryCache,
     NormalizedCacheObject,
-    ApolloProvider
+    ApolloProvider,
+    useQuery
   } from '@apollo/client';
-  import React from 'react';
-  import ReactDOM from 'react-dom';
-  import Pages from './pages';
-  import injectStyles from './styles';
-  
-  const client: ApolloClient<NormalizedCacheObject> = new ApolloClient({
-    cache,
-    link: new HttpLink({
-      uri: 'http://localhost:4000/graphql',
-  
-      headers: {
-        authorization: localStorage.getItem('token'),
-      },
-    }),
-  });
-  
-  
-  cache.writeData({
-    data: {
-      isLoggedIn: !!localStorage.getItem('token'),
-      cartItems: [],
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Pages from './pages';
+import Login from './pages/login';
+import injectStyles from './styles';
+import { resolvers, typeDefs } from './resolvers';
+
+const client: ApolloClient<NormalizedCacheObject> = new ApolloClient({
+  cache,
+  link: new HttpLink({
+    uri: 'http://localhost:4000/graphql',
+
+    headers: {
+      authorization: localStorage.getItem('token'),
     },
-  });
+  }),
+  typeDefs,
+  resolvers,
+});
   
   
-  injectStyles();
-  ReactDOM.render(
-    <ApolloProvider client={client}>
-      <Pages />
-    </ApolloProvider>,
-    document.getElementById('root')
-  );
+cache.writeData({
+  data: {
+    isLoggedIn: !!localStorage.getItem('token'),
+    cartItems: [],
+  },
+});
+
+const IS_LOGGED_IN = gql`
+  query IsUserLoggedIn {
+    isLoggedIn @client
+  }
+`
+;
+function IsLoggedIn() {
+  const { data } = useQuery(IS_LOGGED_IN);
+  return data.isLoggedIn ? <Pages /> : <Login />;
+}
+
+injectStyles();
+ReactDOM.render(
+  <ApolloProvider client={client}>
+    <IsLoggedIn />
+  </ApolloProvider>,
+  document.getElementById('root'),
+);
   
--- start/client/src/pages/cart.tsx
+++ start/client/src/pages/cart.tsx
@@ -1,12 +1,14 @@
-import React from 'react';
+import React, { Fragment } from 'react';
+import { gql, useQuery } from '@apollo/client';
+
+import { Header, Loading } from '../components';
+import { CartItem, BookTrips } from '../containers';
 import { RouteComponentProps } from '@reach/router';
+import * as GetCartItemsTypes from './__generated__/GetCartItems';
 
-interface CartProps extends RouteComponentProps {
-
-}
-
-const Cart: React.FC<CartProps> = () => {
-  return <div />;
-}
-
-export default Cart;
+export const GET_CART_ITEMS = gql`
+  query GetCartItems {
+    cartItems @client
+  }
+`;
+;
